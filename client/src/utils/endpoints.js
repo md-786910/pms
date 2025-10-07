6cd05b56df68b6e26ee56ed1903b50415c71940a
md-786910 <mdashifreza111@gmail.com>
2025-10-07 14:04:07 +0530

    update .env!

--- client/src/utils/endpoints.js
+++ client/src/utils/endpoints.js
@@ -1,7 +1,5 @@
-console.log(process.env);
-
 const API_URL =
-  process.env.REACT_APP_NODE_ENV === "production"
+  process.env.NODE_ENV === "production"
     ? process.env.REACT_APP_API_URL
     : "http://localhost:5000";
 
